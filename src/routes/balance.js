9fc52a2d1810d5a4042bbdf25aeab2311297335b
Rafael Ferreira Umbelino <umbelino12897@gmail.com>
2020-09-08 18:16:44 -0300

    fix type of balance on get balance

--- src/routes/balance.js
+++ src/routes/balance.js
@@ -12,13 +12,14 @@ const router = Router();
 router.get('/balance', (req, res) => {
   try {
     const { account_id } = req.query;
+
     const account = Account.findById(account_id);
 
     if (!account) {
       return res.status(STATUS_NOT_FOUND).send(MESSAGE_NOT_FOUND);
     }
 
-    res.send(account.balance);
+    res.send(String(account.balance));
   } catch ({ message }) {
     res.status(STATUS_ERROR).send({ message });
   }
