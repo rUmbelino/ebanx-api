137431117969e8b3ce2bb452913d8a911971d336
Rafael Ferreira Umbelino <umbelino12897@gmail.com>
2020-09-08 15:37:14 -0300

    creation of constants to respond requests

---
+++ src/routes/constants.js
@@ -0,0 +1,6 @@
+module.exports = {
+  MESSAGE_OK: 'OK',
+  MESSAGE_NOT_FOUND: '0',
+  STATUS_CREATED: 201,
+  STATUS_NOT_FOUND: 404,
+};
--- src/routes/reset.js
+++ src/routes/reset.js
@@ -1,11 +1,13 @@
 const { Router } = require('express');
+
+const { MESSAGE_OK } = require('./constants');
 const Storage = require('../models/Storage');
 
 const router = Router();
 
 router.post('/reset', (req, res) => {
   Storage.reset();
-  res.send();
+  res.send(MESSAGE_OK);
 });
 
 module.exports = router;
